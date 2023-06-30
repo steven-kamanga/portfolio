be621018b555960e256f9d136213d24c8a73e35a
drplxrd <kamangasteven00@gmail.com>
2023-06-30 19:46:20 +0200

    sheesh

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -4,16 +4,16 @@ import Contact from './Contact'
 
 const Content = () => { 
   return (
-    <div className="ml-2 text-gray-200">
-    <div className="border border-t-0 border-b-0 border-r-0 border-yellow-500 lg:ml-40 border-x-1">
+    <div className="text-gray-200 ml-2 lg:ml-[-100px] min-w-min">
+    <div className="min-w-0 border border-t-0 border-b-0 border-r-0 border-yellow-500 lg:ml-40 border-x-1">
       <div className="flex flex-col px-5 lg:pl-5 lg:pr-5 lg:justify-start lg:items-start">
-        <h1 className="lg:items-start lg:justify-start text-[40px] font-bold lg:text-5xl">
+        <h1 className="lg:items-start lg:justify-start md:text-[40px] text-[30px] font-bold lg:text-5xl">
           Hi, I&apos;m Steven Kamanga ✋🏾
         </h1>
         <p className="pt-2 lg:pt-5 text-[20px] font-medium lg:text-xl">
           I&apos;m a Computer Science Student and aspiring Software Engineer 
-          <br/>with an expertise in digital craftsmanship of the highest order,
-          <br/>Dive into the captivating universe of pixels, codes and pure mastery.
+          with an expertise in digital craftsmanship of the highest order,
+         Dive into the captivating universe of pixels, codes and pure mastery.
         </p>
         <Contact className="text-gray-200" />
       </div>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,23 @@
+'use client'
 import React from 'react'
 import Content from './Content'
+import { motion } from 'framer-motion';
+
 
 const Hero = () => {
   return (
     <header className="flex flex-col items-center h-screen lg:flex-row justify-evenly">
-      <div className="text-gray-200 font-mono text-[300px] font-extrabold md:text-[650px] lg:pl-10">
-        <span className="text-black bg-yellow-600 ">S</span>K</div>
+      <div className="text-gray-200 font-mono md:text-[460px] text-[300px] font-extrabold lg:text-[650px] lg:pl-10">
+      <motion.span
+          className="text-black bg-yellow-600"
+          whileHover={{
+            scale: [1, 1.2, 1],
+            rotateY: [0, 180, 0],
+            transition: { duration: 0.5 }
+          }}
+        >
+          S
+        </motion.span>K</div>
       <Content />
     </header>
   )

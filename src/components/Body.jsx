6d446f9e906c1722a147bebe4653c31d42510492
steven-kamanga <kamangasteven00@gmail.com>
2023-08-17 00:07:22 +0200

    Merge remote-tracking branch 'origin/main' into main
    # Conflicts:
    #	src/components/Body.jsx
    #	src/components/Form.jsx

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import ClientHome from './ClientHome'
 import Hero from './Hero'
 import Projects from './Projects'
-import Languages from './Languages'
+// import Languages from './Languages'
 import Somework from './Somework'
-import Footer from './Footer'
+// import Footer from './Footer'
 import Stats from "@/components/Stats";
 
 const Body = () => {
@@ -14,7 +14,7 @@ const Body = () => {
     <Hero />
     <Stats />
     <Projects />
-    <Languages />
+    {/*<Languages />*/}
     <Somework />
     </>
   )
